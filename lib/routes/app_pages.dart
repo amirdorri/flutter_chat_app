9d7e09dbe40acbc36eaa83e4e4b30b8d593360b3
Amir <amirdorri81@gmail.com>
2026-06-29 11:11:12 +0330

    initial commit

---
+++ lib/routes/app_pages.dart
@@ -0,0 +1,89 @@
+
+import 'package:chat_app/routes/app_routes.dart';
+import 'package:chat_app/views/SplashScreen.dart';
+import 'package:get/get.dart';
+
+class AppPages {
+  static const initial = AppRoutes.splash;
+  static final routes = [
+    GetPage(
+      name: AppRoutes.splash,
+      page: () => const SplashScreen(),
+    ),
+  //   GetPage(
+  //     name: AppRoutes.home,
+  //     page: () => const HomeScreen(),
+  // binding: BindingsBuilder((){
+  // Get.put(HoemController());
+  // }
+  //   ),
+  //   GetPage(
+  //     name: AppRoutes.main,
+  //     page: () => const MainScreen(),
+  // binding: BindingsBuilder((){
+  // Get.put(MainController());
+  // }
+  //   ),
+  //   GetPage(
+  //     name: AppRoutes.login,
+  //     page: () => const LoginScreen(),
+  //   ),
+  //   GetPage(
+  //     name: AppRoutes.register,
+  //     page: () => const RegisterScreen(),
+  //   ),
+  //   GetPage(
+  //     name: AppRoutes.forgotPassword,
+  //     page: () => const ForgotPasswordScreen(),
+  //   ),
+  //   GetPage(
+  //     name: AppRoutes.changePassword,
+  //     page: () => const ChangePasswordScreen(),
+  //   ),
+  //   GetPage(
+  //     name: AppRoutes.profile,
+  //     page: () => const ProfileScreen(),
+  // binding: BindingsBuilder((){
+  // Get.put(ProfileController());
+  // }
+  //   ),
+  //   GetPage(
+  //     name: AppRoutes.chat,
+  //     page: () => const ChatScreen(),
+  // binding: BindingsBuilder((){
+  // Get.put(ChatController());
+  // }
+  //   ),
+  //   GetPage(
+  //     name: AppRoutes.usersList,
+  //     page: () => const UsersListScreen(),
+  // binding: BindingsBuilder((){
+  // Get.put(UserListController());
+  // }
+  //   ),
+  //   GetPage(
+  //     name: AppRoutes.friends,
+  //     page: () => const FriendsScreen(),
+  // binding: BindingsBuilder((){
+  // Get.put(FriendsController());
+  // }
+  //
+  //   ),
+  //   GetPage(
+  //     name: AppRoutes.friendsRequests,
+  //     page: () => const FriendsRequestsScreen(),
+  // binding: BindingsBuilder((){
+  // Get.put(FriendsRequestController());
+  // }
+  //   ),
+  //   GetPage(
+  //     name: AppRoutes.notifications,
+  //     page: () => const NotificationsScreen(),
+  //     binding: BindingsBuilder((){
+  //       Get.put(NotificationsController());
+  //     }
+  //
+  //     )
+  //   ),
+  ];
+}
