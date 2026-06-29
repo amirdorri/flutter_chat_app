cbe1190d09a12f10f3b2701c0331b7528bed15cd
Amir <amirdorri81@gmail.com>
2026-06-29 17:11:30 +0330

    splashScreen initial

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
-import 'package:chat_app/firebase_options.dart';
 import 'package:chat_app/routes/app_pages.dart';
 import 'package:chat_app/theme/app_theme.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
+import 'firebase_options.dart';
 
 void main() async {
 
